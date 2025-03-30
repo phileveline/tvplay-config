380cc6447002991e87baccb4f959ceccf6233381
phileveline <108520717+phileveline@users.noreply.github.com>
2025-03-30 14:30:09 +0800

    m.345iptv.com.js

--- m.345iptv.com.js
+++ m.345iptv.com.js
@@ -1,4 +1,1 @@
 document.getElementsByTagName('video')[0].style.position = 'fixed'
-document.querySelector(".nav_wrapper_bg").style.display = 'none'
-document.querySelector(".header_nav").style.display = 'none'
-document.querySelector(".video_right").style.display = 'none'
